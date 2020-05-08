f76424ba3219b83d92ac7cf15d319c9d44d1827c
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2020-05-08 00:03:17 -0500

    cameras and animation

--- js/event-handler.js
+++ js/event-handler.js
@@ -24,37 +24,3 @@ function initEventHandler(e)
 {
 	document.addEventListener("change", onModeChange)
 }
-
-class Sphere extends THREE.Mesh{
-  constructor(r=1, ws=32, hs=32){
-    super();
-    this.geometry = new THREE.SphereGeometry(r, ws, hs);
-    this.material = new THREE.MeshBasicMaterial( {color: 0xffffff} );
-    this.mesh = new THREE.Mesh(this.geometry, this.material)
-    this.repr = "Sphere"
-
-    this.toString = this.toString.bind(this);
-  }
-
-  updateTransform(pos, rot, scale){
-    updatePosition(pos[0], pos[1], pos[2]);
-    updateRotation(rot[0], rot[1], rot[2]);
-    updateScale(scale[0], scale[1], scale[2]);
-  }
-  
-  updatePosition(x, y, z){
-
-  }
-
-  updateRotation(x, y, z){
-
-  }
-
-  updateScale(x, y, z){
-    this.mesh.scale(x,y,z)
-  }
-
-  toString(){
-    return this.repr;
-  }
-}
--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ class Program {
         // this.createCamera = this.createCamera.bind(this);
         this.addMesh = this.addMesh.bind(this);
         this.update = this.update.bind(this);
+        this.createPerspectiveCamera = this.createPerspectiveCamera.bind(this);
+        this.createOrtoCamera = this.createOrtoCamera.bind(this)
         // Auto init
         this.__restart__()
     }
@@ -38,7 +40,7 @@ class Program {
         this.threeRenderer.setSize(this.canvas.width, this.canvas.height);
         this.threeRenderer.setClearColor("black");
         this.scene = new THREE.Scene();
-        this.camera = this.createCamera()
+        this.createPerspectiveCamera()
         this.scene.add(this.camera);
         this.scene.add(this.createLight());
         this.objectsInScene = []
@@ -51,10 +53,21 @@ class Program {
         return new THREE.AmbientLight();
     }
 
-    createCamera(fov=60., near=0.01, far=10000, x=0., y=0., z=5.){
-        var camera = new THREE.PerspectiveCamera(fov, canvas.width / canvas.height, near, far);
+    createPerspectiveCamera(fov=60., near=0.01, far=10000, x=0., y=0., z=5.){
+        var camera = new THREE.PerspectiveCamera(fov, this.canvas.width / this.canvas.height, near, far);
         camera.position.set(x, y, z);
-        return camera
+        this.camera = camera
+    }
+
+    createOrtoCamera(left=(this.canvas.width / - 2), 
+                    right=(this.canvas.width / 2), 
+                    top=(this.canvas.height / 2),
+                    bottom=((this.canvas.height / -22)), 
+                    near=0.01, 
+                    far=10000){
+        var camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+        camera.position.set(x,y,z)
+        this.camera = camera
     }
 
     addMesh(obj){
@@ -67,14 +80,8 @@ class Program {
     update(){
         if (this.sceneReady) {
             this.threeRenderer.render(this.scene, this.camera);
-            
-            // Render all objects
-            //for (var obj in this.objectsInScene){
-                
-            //}
         }
         requestAnimationFrame(this.update);
-    
     }
 }
 
